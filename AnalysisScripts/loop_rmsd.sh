528af962ba7aaacdbc80740d89b3f86154b92c2b
Bala Desinghu <dmbala@gmail.com>
2021-12-08 20:06:20 -0500

    1

--- AnalysisScripts/loop_rmsd.sh
+++ AnalysisScripts/loop_rmsd.sh
@@ -5,10 +5,10 @@ module load ai-fold
 source activate af2
 
 cp  1CBN.pdb input1.pdb
-for i in af2*.pdb rf*.pdb
+for i in ranked*.pdb
 do
     echo -n  "$j - $i  : "
     cp $i input2.pdb
     python ./rmsd.py  > rmsd_ranked_pdbs.dat
 done
-rm input?.pdb 
+rm input?.pdb
